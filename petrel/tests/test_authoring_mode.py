6ccd78462fa05af1335c6253132a0d7b3c32e8b1
dbaty <damien.baty@gmail.com>
2010-11-22 17:54:05 +0000

    Added 'authoring mode' middleware.
    git-svn-id: svn+ssh://noherring.com/home/svn/code/Petrel@7 4c891ba6-aaa7-4e92-8beb-e0e8eb83c35b

---
+++ petrel/tests/test_authoring_mode.py
@@ -0,0 +1,45 @@
+"""Test ``AuthoringModeMiddleware``.
+
+$Id$
+"""
+
+from unittest import TestCase
+
+
+class TestAuthoringMode(TestCase):
+
+    def _make_one(self, *args, **kwargs):
+        from petrel.authoringmode import make_middleware
+        return make_middleware(*args, **kwargs)
+
+    def _call_fut(self, environ, *mw_args, **mw_kwargs):
+        class FakeApp(object):
+            def __call__(self, environ, start_response):
+                return environ
+        app = FakeApp()
+        mw = self._make_one(app=app, global_conf=None,
+                            *mw_args, **mw_kwargs)
+        return mw(environ, start_response=None)
+
+    def test_middleware_authoring_mode(self):
+        environ = {'PATH_INFO': '/authoring/foo/bar'}
+        got = self._call_fut(environ)
+        self.assertEqual(got['PATH_INFO'], '/foo/bar')
+        self.assertEqual(got['authoring_mode'], True)
+
+    def test_middleware_not_in_authoring_mode(self):
+        environ = {'PATH_INFO': '/foo/bar'}
+        got = self._call_fut(environ)
+        self.assertEqual(got, environ)
+
+    def test_custom_environ_key(self):
+        environ = {'PATH_INFO': '/authoring/foo/bar'}
+        got = self._call_fut(environ, environ_key='management_mode')
+        self.assertEqual(got['PATH_INFO'], '/foo/bar')
+        self.assertEqual(got['management_mode'], True)
+
+    def test_custom_prefix(self):
+        environ = {'PATH_INFO': '/manage/foo/bar'}
+        got = self._call_fut(environ, prefix='/manage')
+        self.assertEqual(got['PATH_INFO'], '/foo/bar')
+        self.assertEqual(got['authoring_mode'], True)
